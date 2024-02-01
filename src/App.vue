40faa821789004abae3929ae3b22e7a20904d885
JuZae <julian.zaehring@web.de>
2024-02-01 14:26:34 +0100

    otherTest

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import { defineComponent, provide } from "vue";
 export default defineComponent({
   name: "App",
   setup() {
-    provide("BACKEND_BASE_URL", "http://apoint-backend2:8080/");
+    provide("BACKEND_BASE_URL", "http://apoint-backend:8080/");
   },
 });
 </script>
